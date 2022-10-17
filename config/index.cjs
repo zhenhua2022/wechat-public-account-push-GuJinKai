fca65cd6fcc6626f859545a44a4cc850227424eb
zhenhua2022 <97028255+zhenhua2022@users.noreply.github.com>
2022-10-17 18:02:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,7 +31,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-09-26' },
       ]
     }
   ],
