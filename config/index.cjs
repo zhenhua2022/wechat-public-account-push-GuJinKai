7f092bd4a973321455196a636f25ebaaa328e232
zhenhua2022 <97028255+zhenhua2022@users.noreply.github.com>
2022-10-17 20:34:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       //id: 'oNKGd5x4UCYEQ7YoiY0rw4dPAN-c',//夏
       id: 'oNKGd569zIxI6odnyJDSpgrjqgVo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: -sMFkVIFtXfLoIlcHNS8d8qrSiEBJk_luKcSxZTxc0Q',
+      useTemplateId: '-sMFkVIFtXfLoIlcHNS8d8qrSiEBJk_luKcSxZTxc0Q',
       
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-22',
