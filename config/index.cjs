93b7c6016b768fa5c4afbb71e25fd5c719e01c3b
zhenhua2022 <97028255+zhenhua2022@users.noreply.github.com>
2022-10-17 17:37:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,83 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx068ae71002eb72ff',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '0141c207525b78f0d0c15da1def6c950',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '广西',
+  CITY: '桂林',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '憨憨夏',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oNKGd5x4UCYEQ7YoiY0rw4dPAN-c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'ox0zTzl3176yyFfX74n3KUGIWMM2iY43X1ysNkark_E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
+      horoscopeDate: '03-22',
+      horoscopeDateType: '今日',
+      openUrl: 'https://s.weibo.com/top/summary?cate=realtimehot',
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
       ],
+      courseSchedule: {
+        // 课表
+        courses: {
+            // 例子，周一的课表
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周二
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周三
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周四
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周五
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周六
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周日
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+        }
+      },
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'z7kdpfT5oiE2IeeOtBR9Nw9f7KvzlahRq7WmofLuRgs',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oNKGd569zIxI6odnyJDSpgrjqgVo',
     }
   ],
 
