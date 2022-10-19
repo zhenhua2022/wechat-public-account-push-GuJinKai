ecb8f8aa0e69cf931ab8b5c2de92f477c41716cd
zhenhua2022 <97028255+zhenhua2022@users.noreply.github.com>
2022-10-19 12:33:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,8 +23,8 @@ const USER_CONFIG = {
       name: '憨憨夏',
       
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      //id: 'oNKGd5x4UCYEQ7YoiY0rw4dPAN-c',//user
-      id: 'oNKGd569zIxI6odnyJDSpgrjqgVo',
+      id: 'oNKGd5x4UCYEQ7YoiY0rw4dPAN-c',//user
+      //id: 'oNKGd569zIxI6odnyJDSpgrjqgVo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'iR8HoABn-GIpr6SJ2c0wb3CCjmkbpOs0pkRlmoxoXWQ',
       
