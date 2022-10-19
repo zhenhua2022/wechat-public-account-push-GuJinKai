92b295254c75a5ca30ade9118ae035c1fb34c499
zhenhua2022 <97028255+zhenhua2022@users.noreply.github.com>
2022-10-19 12:07:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -97,7 +97,7 @@ const USER_CONFIG = {
       //id: 'oNKGd5x4UCYEQ7YoiY0rw4dPAN-c',//夏
       id: 'oNKGd569zIxI6odnyJDSpgrjqgVo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'gwuMcpk4XRiJ7H854z9h-_-mZ3CjP6n_3OZM7LkTYGs',
+      useTemplateId: 'AKeeHFHTiHP70tKkccw7mgb78LFLPifoQ-4hcSwd82M',
       
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
